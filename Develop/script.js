fa62a7c3d5a34f74aeb75512bbd5e4851de1229c
Mark Anthony <manthonyamaro@yahoo.com>
2023-04-29 22:04:54 -0500

    added required variable types and function for requesting password length when creating a prompt

---
+++ Develop/script.js
@@ -0,0 +1,28 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+function writePassword() 
+  var length = parseInt(prompt("How long would you like your password to be? (8-128 characters)"));
+
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Password length must be a number between 8 and 128.");
+    return;
+  }
+/*added variables to confirm types*/
+var uppercase = confirm("Would you like to include uppercase letters?");
+var lowercase = confirm("Would you like to include lowercase letters?");
+var numbers = confirm("Would you like to include numbers?");
+var symbols = confirm("Would you like to include symbols?");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
